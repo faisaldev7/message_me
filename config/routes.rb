7692e05f883410dc0ebe4c50927aea26aa97eab6
Faisal Anwar Nizami <faisaldev7@gmail.com>
2020-11-05 21:38:39 +0530

    Generate Chatroom channel and modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'chatroom#index'
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
 
 end
